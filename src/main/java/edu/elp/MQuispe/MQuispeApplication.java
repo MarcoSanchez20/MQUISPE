cfb646c3526c36d751cc0ca08fe6ba4c36df8279
marcosanchez20 <kalos_2511@outlook.com>
2022-05-25 20:02:50 -0500

    examen 3

---
+++ src/main/java/edu/elp/MQuispe/MQuispeApplication.java
@@ -0,0 +1,13 @@
+package edu.elp.MQuispe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MQuispeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MQuispeApplication.class, args);
+	}
+
+}
